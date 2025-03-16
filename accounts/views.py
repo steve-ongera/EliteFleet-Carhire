52cb5069ea3997e30d18cc48ef76edf67cf0a583
steve-ongera <gadafisteve001@gmail.com>
2025-03-17 00:13:14 +0300

    made changes on signup view so that one can automatically activated accounts upons signup

--- accounts/views.py
+++ accounts/views.py
@@ -35,31 +35,37 @@ def register(request):
             email = form.cleaned_data['email']
             password = form.cleaned_data['password']
             username = email.split("@")[0]
-            user = Account.objects.create_user(first_name=first_name, last_name=last_name, email=email, username=username, password=password)
+            
+            user = Account.objects.create_user(
+                first_name=first_name, 
+                last_name=last_name, 
+                email=email, 
+                username=username, 
+                password=password
+            )
             user.phone_number = phone_number
+            user.is_active = True  # Automatically activate the account
             user.save()
 
-            # USER ACTIVATION
-            current_site = get_current_site(request)
-            mail_subject = 'Please activate your account'
-            message = render_to_string('accounts/account_verification_email.html', {
+            # Send welcome email (no activation required)
+            mail_subject = 'Welcome to Our Website!'
+            message = render_to_string('accounts/account_welcome_email.html', {
                 'user': user,
-                'domain': current_site,
-                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
-                'token': default_token_generator.make_token(user),
             })
             to_email = email
             send_email = EmailMessage(mail_subject, message, to=[to_email])
             send_email.send()
-            messages.success(request, 'Thank you for registering with us. We have sent you a verification email to your email address [rathan.kumar@gmail.com]. Please verify it.')
-            return redirect('/accounts/login/?command=verification&email='+email)
+
+            messages.success(request, 'Your account has been created and activated successfully! You can now log in.')
             return redirect('login')
     else:
         form = RegistrationForm()
+
     context = {
         'form': form,
     }
     return render(request, 'accounts/register.html', context)
+
 
 
 def login(request):
